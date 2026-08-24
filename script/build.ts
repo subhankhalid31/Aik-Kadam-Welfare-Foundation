f18ae448e37dd89a74b39d1b1ba41ae0d02bb6c8
subhankhalid31 <subhankhalid8787@gmail.com>
2026-08-24 17:00:19 +0500

    Minor changes

--- script/build.ts
+++ script/build.ts
@@ -2,7 +2,7 @@ import { build } from "esbuild";
 import { execSync } from "child_process";
 
 console.log("Building client...");
-execSync("vite build", { stdio: "inherit" });
+execSync("npx vite build", { stdio: "inherit" });
 
 console.log("Building server...");
 await build({
